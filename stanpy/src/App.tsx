ff4ec0c812cdf066fc7a5422ac3d8393504259ee
Stanley John O. Pangaruy <pangaruystanley@GMAIL.COM>
2025-06-03 16:26:45 +0800

    fixedd

--- stanpy/src/App.tsx
+++ stanpy/src/App.tsx
@@ -11,14 +11,14 @@ function App() {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
-    const handleMouseMove = (e) => {
+    const handleMouseMove = (e: MouseEvent) => {
       setMousePosition({ x: e.clientX, y: e.clientY });
     };
     window.addEventListener('mousemove', handleMouseMove);
     return () => window.removeEventListener('mousemove', handleMouseMove);
   }, []);
 
-  const scrollToSection = (sectionId) => {
+  const scrollToSection = (sectionId: string) => {
     document.getElementById(sectionId)?.scrollIntoView({ behavior: 'smooth' });
     setActiveSection(sectionId);
     setIsMenuOpen(false);
